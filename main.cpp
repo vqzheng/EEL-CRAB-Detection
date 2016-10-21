2b989c93fdd6c6c5b5793bf3c73ab68071cc829d
vqzheng <vqzheng@bu.edu>
2016-10-20 22:09:18 -0400

    canny and edge test

--- main.cpp
+++ main.cpp
@@ -6,19 +6,28 @@
 
 int main(int argc, char** argv)
 {
-	const std::string source = ".../eel1.avi";
+	const std::string source = "eel1.avi";
 	cv::VideoCapture inputVideo(source);
 	if (!inputVideo.isOpened()){
 		std::cout << "Could not open the input video: " << source << std::endl;
 		return -1;
 	}
-	cv::Mat raw;
+	cv::Mat edge;
 	cv::namedWindow("source", CV_WINDOW_AUTOSIZE);
+	cv::namedWindow("edge", CV_WINDOW_AUTOSIZE);
+	cv::namedWindow("test", CV_WINDOW_AUTOSIZE);
+	cv::Ptr<cv::BackgroundSubtractor> pMOG2 = cv::createBackgroundSubtractorMOG2();
 
 	for (;;){
 		cv::Mat frame;
 		inputVideo >> frame;
+		cvtColor(frame, edge, CV_BGR2GRAY);
+		cv::GaussianBlur(edge, edge, cv::Size(9, 9), 1.5, 1.5);
+		pMOG2->apply(edge, frame);
+		cv::Canny(edge, edge, 0, 45, 3);
 		cv::imshow("source", frame);
+		cv::imshow("edge", edge);
+
 		if (cv::waitKey(30) >= 0) break;
 	}
 	
